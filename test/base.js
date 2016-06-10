8f26f9ed398257474f7f15fce0d35ba0be588a13
Max Holman <max@holmn.com>
2016-06-10 14:55:50 +0800

    extra test for allowing perms akin to `company:*`

--- test/base.js
+++ test/base.js
@@ -432,4 +432,17 @@ describe('Base', function () {
             .catch(done);
     });
 
+
+    it('Access Control using patternProperties to allow an entire namespace', function (done) {
+
+        var permission = acWithObjects();
+
+        permission.can('payment:something', 33)
+            .then(function (valid) {
+                assert.isNotOk(valid);
+                done();
+            })
+            .catch(done);
+    });
+
 });
